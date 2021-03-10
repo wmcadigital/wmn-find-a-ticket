da284c9ac1bfa6b971729c3dec827b71d9372add
houbly <h.blyden@gmail.com>
2021-03-10 09:42:58 +0000

    update content

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 // Contexts
 import ContextProvider from 'globalState/ContextProvider';
-// Components
-import Icon from 'components/shared/Icon/Icon';
-// Styles
-import s from './App.module.scss';
 
 function App() {
   return (
@@ -12,32 +8,8 @@ function App() {
       <ContextProvider>
         <div className="wmnds-container wmnds-p-t-lg wmnds-p-b-lg wmnds-grid">
           <div className="wmnds-col-1">
-            <h2>Hello West Midlands!</h2>
-            <p>Don&apos;t forget to:</p>
-            <ul>
-              <li>
-                Update the variables in <strong>.env</strong> file
-              </li>
-              <li>
-                Update the <strong>breadcrumbs</strong>
-              </li>
-            </ul>
-
-            {/* Bus */}
-            <Icon
-              className={`${s.icon} wmnds-col-1 wmnds-col-sm-1-3`}
-              iconName="modes-isolated-bus"
-            />
-            {/* Metro */}
-            <Icon
-              className={`${s.icon} wmnds-col-1 wmnds-col-sm-1-3`}
-              iconName="modes-isolated-metro"
-            />
-            {/* Train */}
-            <Icon
-              className={`${s.icon} wmnds-col-1 wmnds-col-sm-1-3`}
-              iconName="modes-isolated-rail"
-            />
+            <h1>Find a ticket</h1>
+            <p>Find and buy day, week and season tickest based on the way you travel.</p>
           </div>
         </div>
       </ContextProvider>
