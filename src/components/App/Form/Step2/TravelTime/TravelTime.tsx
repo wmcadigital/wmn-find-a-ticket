5892e6c01c8cf392ced8b2fd7d7d3b48a5a5a925
houbly <h.blyden@gmail.com>
2021-03-19 14:43:42 +0000

    added travel time question

--- src/components/App/Form/Step2/TravelTime/TravelTime.tsx
+++ src/components/App/Form/Step2/TravelTime/TravelTime.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import Radios from '../../../../shared/Radios/Radios';
+import Button from '../../../../shared/Button/Button';
 
 const TravelTime = () => {
+  const radioOptions = [
+    {
+      name: 'travelTime',
+      text: '<strong>Peak</strong><br>I need to travel before 9.30am',
+      value: 'peak',
+    },
+    {
+      name: 'travelTime',
+      text: '<strong>Off-peak</strong><br>I will only travel after 9.30am',
+      value: 'offpeak',
+    },
+    {
+      name: 'travelTime',
+      text:
+        '<strong>Disabled or Older Person’s pass holder</strong><br>I need to travel before 9.30am ',
+      value: 'senior',
+    },
+  ];
   return (
     <div className="bg-white wmnds-p-lg wmnds-m-b-lg">
-      <h2 className="wmnds-fe-question">Will you travel during peak hours?</h2>
+      <Radios
+        name="travelTime"
+        label="Will you travel during peak hours?"
+        hint="Sometimes you can get a cheaper ticket if you only travel after 9.30am. "
+        radios={radioOptions}
+      />
+      <Button text="Continue" />
     </div>
   );
 };
