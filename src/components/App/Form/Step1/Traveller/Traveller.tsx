1b701a76a1cb6b999270d768187850df2b2b5bfe
houbly <h.blyden@gmail.com>
2021-03-19 12:30:41 +0000

    Add traveller radios

--- src/components/App/Form/Step1/Traveller/Traveller.tsx
+++ src/components/App/Form/Step1/Traveller/Traveller.tsx
@@ -1,12 +1,23 @@
 import React, { useContext } from 'react';
 import { FormContext } from '../../../../../globalState';
+import Radios from '../../../../shared/Radios/Radios';
+import Button from '../../../../shared/Button/Button';
 
 const Traveller = () => {
   const [formState] = useContext(FormContext);
+  const radioOptions = [
+    { name: 'traveller', text: 'Adult', value: 'adult' },
+    { name: 'traveller', text: 'Young person (5-18)', value: 'youngPerson' },
+    { name: 'traveller', text: 'Student (16+)', value: 'student' },
+    { name: 'traveller', text: 'Group or Family', value: 'group' },
+    { name: 'traveller', text: 'Older person', value: 'senior' },
+    { name: 'traveller', text: 'Disabled person', value: 'disabled' },
+  ];
   return (
     <div className="bg-white wmnds-p-lg wmnds-m-b-lg">
-      <h2 className="wmnds-fe-question">Who will be travelling?</h2>
       {formState.ticketInfo.traveller}
+      <Radios name="traveller" label="Who will be travelling?" radios={radioOptions} />
+      <Button text="Continue" />
     </div>
   );
 };
