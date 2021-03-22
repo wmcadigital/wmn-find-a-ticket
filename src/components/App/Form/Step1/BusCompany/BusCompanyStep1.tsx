8db43c251c2052af700986cdd012728b3950ae40
houbly <h.blyden@gmail.com>
2021-03-22 09:58:01 +0000

    add radio functionality buscompany step 1

--- src/components/App/Form/Step1/BusCompany/BusCompanyStep1.tsx
+++ src/components/App/Form/Step1/BusCompany/BusCompanyStep1.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { FormContext } from '../../../../../globalState';
 import Radios from '../../../../shared/Radios/Radios';
 import Button from '../../../../shared/Button/Button';
 
 const Step1 = () => {
+  const [, formDispatch] = useContext(FormContext);
+  const [state, setState] = useState('');
   const radioOptions = [
     { name: 'busNetwork', text: 'Yes', value: 'yes' },
     { name: 'busNetwork', text: 'No', value: 'no' },
   ];
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setState(e.target.value);
+  };
+
+  const handleContinue = () => {
+    formDispatch({ type: 'UPDATE_TICKET_TYPE', payload: state === 'yes' ? 'nBus' : 'single' });
+  };
   return (
     <div className="bg-white wmnds-p-lg wmnds-m-b-lg">
       <Radios
@@ -14,11 +24,12 @@ const Step1 = () => {
         hint="Travel on any bus company in the West Midlands Network"
         label="Do you want to travel on any bus?"
         radios={radioOptions}
+        onChange={handleChange}
       />
       <div className="wmnds-p-b-lg">
         <Button btnClass="wmnds-btn--link" text="I don't know which bus I need" />
       </div>
-      <Button text="Continue" />
+      <Button text="Continue" onClick={handleContinue} />
     </div>
   );
 };
