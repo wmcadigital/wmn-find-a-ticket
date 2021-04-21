5087ebdcd634692d186da245e26c86eb29c8c6e9
houbly <h.blyden@gmail.com>
2021-04-21 09:01:14 +0100

    add mode state to icons

--- src/components/App/Form/Step4/Purchase/Purchase.tsx
+++ src/components/App/Form/Step4/Purchase/Purchase.tsx
@@ -1,20 +1,39 @@
 import QuestionCard from 'components/shared/QuestionCard/QuestionCard';
 import Icon from 'components/shared/Icon/Icon';
 import Button from 'components/shared/Button/Button';
+import { useFormContext } from 'globalState';
 import s from './Purchase.module.scss';
 
 // Purchase Journey (TO DO)
 const Purchase = () => {
+  const [formState] = useFormContext();
+  const { ticketInfo } = formState;
+
+  const iconText = (mode: string) => {
+    let icon = mode;
+    if (mode === 'tram') {
+      icon = 'metro';
+    } else if (mode === 'train') {
+      icon = 'rail';
+    }
+    return icon;
+  };
   return (
     <div className="wmnds-grid wmnds-grid--spacing-md-2-md">
       <div className="wmnds-col-1-1 wmnds-col-md-2-3">
         <QuestionCard>
           <div className="wmnds-grid wmnds-grid--spacing-md-2-md">
             <div className="wmnds-col-2-3">
-              <h2>
-                Monthly Direct Debit nBus West Midlands
-                <Icon iconName="modes-isolated-bus" className={`${s.modeIcon} ${s.bus}`} />
-              </h2>
+              <h2 className={s.heading}>Monthly Direct Debit nBus West Midlands</h2>
+              <div className={s.icons}>
+                {ticketInfo.modes?.map((mode: any) => (
+                  <Icon
+                    key={`icon-${mode}`}
+                    iconName={`modes-isolated-${iconText(mode)}`}
+                    className={`${s.modeIcon} ${s[mode]}`}
+                  />
+                ))}
+              </div>
               <h3>£64.00 per month</h3>
             </div>
             <div className="wmnds-col-1-3">
