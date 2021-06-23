13c790de53ca6dc8624f32a7fd0cbd288c52acd5
houbly <h.blyden@gmail.com>
2021-06-23 11:24:31 +0100

    Add text to loader

--- src/components/shared/Loader/Loader.tsx
+++ src/components/shared/Loader/Loader.tsx
@@ -1,9 +1,10 @@
-const Loader = () => {
+const Loader = ({ text }: { text?: string }) => {
   return (
     <div className="wmnds-col-1">
       <div className="wmnds-loader" role="alert" aria-live="assertive">
         <p className="wmnds-loader__content">Content is loading...</p>
       </div>
+      {text && <p className="h4 wmnds-text-align-center">{text}</p>}
     </div>
   );
 };
