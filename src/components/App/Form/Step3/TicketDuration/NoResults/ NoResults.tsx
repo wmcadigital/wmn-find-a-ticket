d74f3289a146a37c156910c33977848b9f9ebcb8
houbly <h.blyden@gmail.com>
2021-10-07 14:29:43 +0100

    add no results page for concessions

---
+++ src/components/App/Form/Step3/TicketDuration/NoResults/ NoResults.tsx
@@ -0,0 +1,43 @@
+const NoResults = () => {
+  return (
+    <div className="wmnds-col-1">
+      <div className="wmnds-p-lg bg-white">
+        <h2>Free travel in the West Midlands</h2>
+        <p>
+          If you have a Disabled person’s pass or an Older person’s pass you can travel off-peak on
+          the bus across the UK. You can also travel off-peak on the train and tram in the{' '}
+          <a href="https://find-rail-zones.wmnetwork.co.uk/" rel="noreferrer" target="_blank">
+            Transport for West Midlands area
+          </a>
+          .
+        </p>
+        <p>Off-peak times are:</p>
+        <ul>
+          <li>9.30am and 11pm, Monday to Friday </li>
+          <li>all day at weekends and on public holidays.</li>
+        </ul>
+        <p className="wmnds-m-none">
+          You can find more information on travel times and eligibility criteria for a{' '}
+          <a
+            href="https://www.tfwm.org.uk/swift-and-tickets/discounts-and-free-travel-passes/get-a-disabled-person-s-pass/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Disabled person&rsquo;s pass
+          </a>{' '}
+          or{' '}
+          <a
+            href="https://www.tfwm.org.uk/swift-and-tickets/discounts-and-free-travel-passes/get-an-older-person-s-travel-pass/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Older person&rsquo;s pass
+          </a>{' '}
+          on our website.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NoResults;
