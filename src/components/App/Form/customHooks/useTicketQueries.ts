6d726ba20fa3d90a3781d4ecb95b20c3ce03a6a9
houbly <h.blyden@gmail.com>
2021-10-11 17:26:23 +0100

    use Id for nbus operator filter

--- src/components/App/Form/customHooks/useTicketQueries.ts
+++ src/components/App/Form/customHooks/useTicketQueries.ts
@@ -29,12 +29,14 @@ const useTicketQueries = () => {
       isFamily: ticketInfo.traveller === 'family',
     };
 
-    // If bus company is null or 'nBus' default to 'West Midlands Combined Authority' as filter
+    // If bus company is null or 'nBus' match id for all operators
     const operatorQuery = {
       operator:
-        ticketInfo.busCompany && ticketInfo.busCompany !== 'nBus'
-          ? ticketInfo.busCompany
-          : 'West Midlands Combined Authority',
+        ticketInfo.busCompany && ticketInfo.busCompany !== 'nBus' ? ticketInfo.busCompany : null,
+      operatorId:
+        !ticketInfo.busCompany || ticketInfo.busCompany === 'nBus'
+          ? '74ab7f8a-7c80-e411-9265-0050568f6584'
+          : null,
     };
 
     // INCLUDES BUS ONLY
