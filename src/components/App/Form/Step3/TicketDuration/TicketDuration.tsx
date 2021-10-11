21ec8ed00c73f753311f0610d9c5f80d70828324
Houston Blyden <h.blyden@gmail.com>
2021-10-11 10:28:26 +0100

    feat: add no results screens (#115)
    * add no results page for concessions
    
    * Add generic no results

--- src/components/App/Form/Step3/TicketDuration/TicketDuration.tsx
+++ src/components/App/Form/Step3/TicketDuration/TicketDuration.tsx
@@ -5,6 +5,7 @@ import QuestionCard, { ChangeAnswers } from 'components/shared/QuestionCard/Ques
 import Button from 'components/shared/Button/Button';
 import questions from '../../questions';
 import useConvertDescription from '../../Step4/customHooks/useConvertDescription';
+import NoResults from './NoResults/ NoResults';
 
 import { Ticket } from '../../types/Tickets.types';
 import s from './TicketDuration.module.scss';
@@ -80,114 +81,110 @@ const TicketDuration = ({
 
   return (
     <>
-      <QuestionCard showChangeBtn={false}>
-        <h2 className="wmnds-fe-question">{question}</h2>
-        <p className="wmnds-m-none">{hint}</p>
-      </QuestionCard>
-      <div className="wmnds-grid wmnds-grid--spacing-sm-2-lg">
+      {results && results!.length > 0 ? (
         <>
-          {results && results!.length > 0 ? (
-            <>
-              {results!
-                .sort((a, b) => a.ticketCurrentAmount - b.ticketCurrentAmount)
-                .map((option: Ticket) => {
-                  const isSale = option.ticketCurrentAmount < option.standardCurrentAmount;
-                  return (
-                    <div key={option.id} className="wmnds-col-1 wmnds-col-sm-1-2 wmnds-m-b-lg">
-                      <div className={`bg-white wmnds-p-md ${s.ticketCard}`}>
-                        {isSale ? (
-                          <div>
-                            <div className={`wmnds-grid ${s.sale}`}>
-                              <h4 className="wmnds-col-auto">
+          <QuestionCard showChangeBtn={false}>
+            <h2 className="wmnds-fe-question">{question}</h2>
+            <p className="wmnds-m-none">{hint}</p>
+          </QuestionCard>
+          <div className="wmnds-grid wmnds-grid--spacing-sm-2-lg">
+            {results!
+              .sort((a, b) => a.ticketCurrentAmount - b.ticketCurrentAmount)
+              .map((option: Ticket) => {
+                const isSale = option.ticketCurrentAmount < option.standardCurrentAmount;
+                return (
+                  <div key={option.id} className="wmnds-col-1 wmnds-col-sm-1-2 wmnds-m-b-lg">
+                    <div className={`bg-white wmnds-p-md ${s.ticketCard}`}>
+                      {isSale ? (
+                        <div>
+                          <div className={`wmnds-grid ${s.sale}`}>
+                            <h4 className="wmnds-col-auto">
+                              {option.buyOnDirectDebit
+                                ? 'Monthly Direct Debit'
+                                : getValidityInfo(option.validityDays)}{' '}
+                              <span className={s.fullPrice}>
+                                £{option.standardCurrentAmount.toFixed(2)}
+                              </span>{' '}
+                              <span className={s.salePrice}>
+                                £{option.ticketCurrentAmount.toFixed(2)}
+                              </span>
+                            </h4>
+                            <div className="wmnds-col-auto">
+                              <div className={s.saleBadge}>Sale</div>
+                            </div>
+                          </div>
+                          <div className="wmnds-m-b-sm">
+                            {option.validityDays === '1' ? (
+                              <span>This is also a daily fare cap on Swift Go.</span>
+                            ) : (
+                              <>
+                                £
+                                {(
+                                  option.ticketCurrentAmount / parseInt(option.validityDays, 10)
+                                ).toFixed(2)}{' '}
+                                per day
+                              </>
+                            )}
+                          </div>
+                        </div>
+                      ) : (
+                        <div>
+                          {option.ticketCurrentAmount === 0 && option.validityDays === '1' ? (
+                            <h4>Swift Go</h4>
+                          ) : (
+                            <h4>
+                              <>
                                 {option.buyOnDirectDebit
                                   ? 'Monthly Direct Debit'
                                   : getValidityInfo(option.validityDays)}{' '}
-                                <span className={s.fullPrice}>
-                                  £{option.standardCurrentAmount.toFixed(2)}
-                                </span>{' '}
-                                <span className={s.salePrice}>
-                                  £{option.ticketCurrentAmount.toFixed(2)}
+                                <span className={s.totalPrice}>
+                                  {' '}
+                                  {option.ticketCurrentAmount > 0 && (
+                                    <>£{option.ticketCurrentAmount.toFixed(2)}</>
+                                  )}
                                 </span>
-                              </h4>
-                              <div className="wmnds-col-auto">
-                                <div className={s.saleBadge}>Sale</div>
-                              </div>
-                            </div>
-                            <div className="wmnds-m-b-sm">
-                              {option.validityDays === '1' ? (
-                                <span>This is also a daily fare cap on Swift Go.</span>
-                              ) : (
-                                <>
-                                  £
-                                  {(
-                                    option.ticketCurrentAmount / parseInt(option.validityDays, 10)
-                                  ).toFixed(2)}{' '}
-                                  per day
-                                </>
-                              )}
-                            </div>
-                          </div>
-                        ) : (
-                          <div>
-                            {option.ticketCurrentAmount === 0 && option.validityDays === '1' ? (
-                              <h4>Swift Go</h4>
+                              </>
+                            </h4>
+                          )}
+                          <div className="wmnds-m-b-sm">
+                            {option.validityDays === '1' ? (
+                              <span>
+                                {option.ticketCurrentAmount > 0
+                                  ? 'This is also a daily fare cap on Swift Go.'
+                                  : 'Capped at either a 1 day, 3 day or weekly charge'}
+                              </span>
                             ) : (
-                              <h4>
-                                <>
-                                  {option.buyOnDirectDebit
-                                    ? 'Monthly Direct Debit'
-                                    : getValidityInfo(option.validityDays)}{' '}
-                                  <span className={s.totalPrice}>
-                                    {' '}
-                                    {option.ticketCurrentAmount > 0 && (
-                                      <>£{option.ticketCurrentAmount.toFixed(2)}</>
-                                    )}
-                                  </span>
-                                </>
-                              </h4>
+                              <>
+                                £
+                                {(
+                                  option.ticketCurrentAmount / parseInt(option.validityDays, 10)
+                                ).toFixed(2)}{' '}
+                                per day
+                              </>
                             )}
-                            <div className="wmnds-m-b-sm">
-                              {option.validityDays === '1' ? (
-                                <span>
-                                  {option.ticketCurrentAmount > 0
-                                    ? 'This is also a daily fare cap on Swift Go.'
-                                    : 'Capped at either a 1 day, 3 day or weekly charge'}
-                                </span>
-                              ) : (
-                                <>
-                                  £
-                                  {(
-                                    option.ticketCurrentAmount / parseInt(option.validityDays, 10)
-                                  ).toFixed(2)}{' '}
-                                  per day
-                                </>
-                              )}
-                            </div>
                           </div>
-                        )}
-                        <Button
-                          btnClass="wmnds-btn--block"
-                          text="Select"
-                          onClick={() => handleContinue(option)}
-                        />
-                      </div>
+                        </div>
+                      )}
+                      <Button
+                        btnClass="wmnds-btn--block"
+                        text="Select"
+                        onClick={() => handleContinue(option)}
+                      />
                     </div>
-                  );
-                })}
-            </>
-          ) : (
-            <div className="wmnds-col-1">
-              <h3>No results</h3>
-            </div>
-          )}
-        </>
+                  </div>
+                );
+              })}
 
-        <div className="wmnds-col-1">
-          <div className="wmnds-hide-desktop">
-            <ChangeAnswers />
+            <div className="wmnds-col-1">
+              <div className="wmnds-hide-desktop">
+                <ChangeAnswers />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <NoResults />
+      )}
     </>
   );
 };
