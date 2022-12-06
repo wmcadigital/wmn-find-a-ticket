4377256b2e453d0dc88c0ccabd2a2a8d99dc53c5
Sudheer Kotha <sudheer.kotha@wmca@org.uk>
2022-12-06 15:25:13 +0000

    tram zone data added

--- src/components/App/Form/Step2/TravelTime/TravelTime.tsx
+++ src/components/App/Form/Step2/TravelTime/TravelTime.tsx
@@ -16,8 +16,15 @@ const TravelTime = () => {
   const { handleChange, handleContinue, genericError, error } = useHandleChange(name);
   const { question, hint, options } = questions[name];
   // eslint-disable-next-line prettier/prettier
-  const { modesQuery, travellerQuery, operatorQuery, busTravelQuery, railZonesQuery, trainQuery } =
-    useTicketQueries();
+  const {
+    modesQuery,
+    travellerQuery,
+    operatorQuery,
+    busTravelQuery,
+    railZonesQuery,
+    tramZonesQuery,
+    trainQuery,
+  } = useTicketQueries();
   let ticketQuery = { ...travellerQuery };
   if (ticketInfo.modes?.includes('bus')) {
     ticketQuery = { ...travellerQuery, ...busTravelQuery, ...operatorQuery };
@@ -25,7 +32,9 @@ const TravelTime = () => {
   if (ticketInfo.modes?.includes('train')) {
     ticketQuery = { ...modesQuery, ...travellerQuery, ...railZonesQuery, ...trainQuery };
   }
-
+  if (ticketInfo.modes?.includes('tram')) {
+    ticketQuery = { ...modesQuery, ...travellerQuery, ...tramZonesQuery, ...trainQuery };
+  }
   useEffect(() => {
     if ((traveller === 'concessionary' || traveller === 'disabled') && !travelTime) {
       formDispatch({
